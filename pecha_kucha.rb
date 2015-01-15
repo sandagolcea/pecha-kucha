0c083bcb87a4296899bfb6d4423e8123c059badb
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-15 18:20:43 +0000

    Core functionality done for 1rst player

--- pecha_kucha.rb
+++ pecha_kucha.rb
@@ -4,21 +4,32 @@ class PechaKucha < Sinatra::Base
   enable :sessions
 
   get '/' do
+    session[:list] = []
     erb :index
   end
 
   post '/' do
-    session[:user1] = params[:user1]
+
+    # puts session.inspect
+    @user1 = params[:user1]
     @user2 = params[:user2]
     @user3 = params[:user3]
     @user4 = params[:user4]
-    @user5 = params[:user5]
+    @user5 = params[:user5]    
+    
 
+    @list = []
+    @list << params[:user1] if ( params[:user1] && !params[:user1].empty? )
+    
+    session[:list] << @list if !@list.empty?
+    puts session.inspect
     erb :index
   end
 
   post '/start' do
-    @user1 = session[:user1]
+    # @user = session[:user]
+    session[:list].shuffle
+    @user = session[:list].pop
     erb :start
   end
 
