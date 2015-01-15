528d5a91a65219a3b282896ccca34ca1f01469ef
Sanda Golcea <sanda.golcea@gmail.com>
2015-01-15 16:45:30 +0000

    Initial commit - basic views created with links

---
+++ config.ru
@@ -0,0 +1,4 @@
+require 'rubygems'
+require File.join(File.dirname(__FILE__), 'pecha_kucha.rb')
+
+run PechaKucha
---
+++ pecha_kucha.rb
@@ -0,0 +1,27 @@
+require 'sinatra/base'
+
+class PechaKucha < Sinatra::Base
+  enable :sessions
+  
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    session[:user1] = params[:user1]
+    @user2 = params[:user2]
+    @user3 = params[:user3]
+    @user4 = params[:user4]
+    @user5 = params[:user5]
+
+    erb :index
+  end
+
+  post '/start' do
+    @user1 = session[:user1]
+    erb :start
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
